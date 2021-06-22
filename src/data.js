38189faca0cbd9d52f75a89bd395f53cb2965537
Sarah Almahmid <hpsarontm@gmail.com>
2021-06-22 16:45:42 +0300

    added pokemon data file

---
+++ src/data.js
@@ -0,0 +1,116 @@
+const pokemon = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    type: ["Grass", "Poison"],
+    imageURL: "https://cdn2.bulbagarden.net/upload/2/21/001Bulbasaur.png",
+  },
+  {
+    id: 2,
+    name: "Ivysaur",
+    type: ["Grass", "Poison"],
+    imageURL: "https://cdn2.bulbagarden.net/upload/7/73/002Ivysaur.png",
+  },
+  {
+    id: 3,
+    name: "Venusaur",
+    type: ["Grass", "Poison"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    type: ["Fire"],
+    imageURL: "https://cdn2.bulbagarden.net/upload/7/73/004Charmander.png",
+  },
+  {
+    id: 5,
+    name: "Charmeleon",
+    type: ["Fire"],
+    imageURL:
+      "https://i.pinimg.com/originals/9e/fe/f7/9efef7d233a80b4ba49bc90baadcb912.png",
+  },
+  {
+    id: 6,
+    name: "Charizard",
+    type: ["Fire", "Flying"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    type: ["Water"],
+    imageURL: "https://cdn2.bulbagarden.net/upload/3/39/007Squirtle.png",
+  },
+  {
+    id: 8,
+    name: "Wartortle",
+    type: ["Water"],
+    imageURL:
+      "https://static.wikia.nocookie.net/legendsofthemultiuniverse/images/2/2d/8-Wartortle.png/revision/latest?cb=20190204023340",
+  },
+  {
+    id: 9,
+    name: "Blastoise",
+    type: ["Water"],
+    imageURL:
+      "https://cdn2.bulbagarden.net/upload/thumb/0/02/009Blastoise.png/500px-009Blastoise.png",
+  },
+  {
+    id: 10,
+    name: "Caterpie",
+    type: ["Bug"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
+  },
+  {
+    id: 11,
+    name: "Metapod",
+    type: ["Bug"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/011.png",
+  },
+  {
+    id: 12,
+    name: "Butterfree",
+    type: ["Bug", "Flying"],
+    imageURL:
+      "https://cdn2.bulbagarden.net/upload/thumb/d/d1/012Butterfree.png/1200px-012Butterfree.png",
+  },
+  {
+    id: 13,
+    name: "Weedle",
+    type: ["Bug", "Poison"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/013.png",
+  },
+  {
+    id: 14,
+    name: "Kakuna",
+    type: ["Bug", "Poison"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/014.png",
+  },
+  {
+    id: 15,
+    name: "Beedrill",
+    type: ["Bug", "Poison"],
+    imageURL:
+      "https://cdn2.bulbagarden.net/upload/thumb/6/61/015Beedrill.png/1200px-015Beedrill.png",
+  },
+  {
+    id: 16,
+    name: "Pidgey",
+    type: ["Normal", "Flying"],
+    imageURL: "https://cdn2.bulbagarden.net/upload/5/55/016Pidgey.png",
+  },
+  {
+    id: 17,
+    name: "Pidgeotto",
+    type: ["Normal", "Flying"],
+    imageURL: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/017.png",
+  },
+  {
+    id: 18,
+    name: "Pidgeot",
+    type: ["Normal", "Flying"],
+    imageURL:
+      "https://cdn2.bulbagarden.net/upload/thumb/5/57/018Pidgeot.png/1200px-018Pidgeot.png",
+  },
+];
